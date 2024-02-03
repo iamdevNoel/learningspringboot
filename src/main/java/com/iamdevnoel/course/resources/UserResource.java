edac2d71588c1c3717cf441bf70896ac7021e5d6
Noberto <contato.gabrielnoberto@gmail.com>
2024-02-03 17:29:43 -0300

    Registra camada de servico e cria método findById no UserRepository

--- src/main/java/com/iamdevnoel/course/resources/UserResource.java
+++ src/main/java/com/iamdevnoel/course/resources/UserResource.java
@@ -1,19 +1,31 @@
 package com.iamdevnoel.course.resources;
 
 import com.iamdevnoel.course.entities.User;
+import com.iamdevnoel.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController // Responde requisições http
 @RequestMapping(value = "/users") // endereço para acessar este endpoint
 public class UserResource {
+    @Autowired
+    private UserService userService;
 
     @GetMapping // informando o verbo HTTP (GET)
-    public ResponseEntity<User> findAll() {
-        User user = new User(1L, "Maria", "maria@gmail.com", "999999999", "12345678");
-        return ResponseEntity.ok().body(user); //
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = userService.findAll();
+        return ResponseEntity.ok().body(list); //
     }
 
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User user = userService.findById(id);
+        return ResponseEntity.ok().body(user);
+    }
 }
---
+++ src/main/java/com/iamdevnoel/course/services/UserService.java
@@ -0,0 +1,25 @@
+package com.iamdevnoel.course.services;
+
+import com.iamdevnoel.course.entities.User;
+import com.iamdevnoel.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> user = userRepository.findById(id);
+        return user.get();
+    }
+}
