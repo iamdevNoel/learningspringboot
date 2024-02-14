a1ec63c8bdb59ae4d260288ad4ad28174b3c71ce
iamdevnoel <contato.gabrielnoberto@gmail.com>
2024-02-14 08:01:07 -0300

    Adiciona tratamento de exceção ao método update() users

--- src/main/java/com/iamdevnoel/course/services/UserService.java
+++ src/main/java/com/iamdevnoel/course/services/UserService.java
@@ -4,6 +4,7 @@ import com.iamdevnoel.course.entities.User;
 import com.iamdevnoel.course.repositories.UserRepository;
 import com.iamdevnoel.course.services.exceptions.DatabaseException;
 import com.iamdevnoel.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -52,8 +53,12 @@ public class UserService {
     }
 
     public User update(Long id, User updatedUser) {
-        User entity = userRepository.getReferenceById(id);
-        updateData(entity, updatedUser);
-        return userRepository.save(entity);
+        try {
+            User entity = userRepository.getReferenceById(id);
+            updateData(entity, updatedUser);
+            return userRepository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 }
